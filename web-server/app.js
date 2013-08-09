4780cdf9ffe7ef1d1bcbe857b41993f510b45c73
zcl <28928400@qq.com>
2013-08-09 18:33:56 +0800

    save

--- web-server/app.js
+++ web-server/app.js
@@ -6,11 +6,10 @@ app.configure(function () {
     app.use(express.methodOverride());
     app.use(express.bodyParser());
     app.use(app.router);
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('views', __dirname + '/public');
     app.set('view options', {layout: false});
-    app.use(express['static'](__dirname + '/public'));
-    //app.set('basepath', __dirname + '/public');
+    app.set('basepath', __dirname + '/public');
 });
 
 app.configure('development', function () {
@@ -23,6 +22,6 @@ app.configure('production', function () {
     app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
     app.use(express.errorHandler());
 });
-require('./router')(app);
+
 console.log("Web server has started.\nPlease log on http://127.0.0.1:3001/index.html");
 app.listen(3001);
--- web-server/router.js
+++
@@ -1,8 +0,0 @@
-var login=require('./routes/login');
-
-module.exports=function(app){
-    app.get('/',login.login);
-    app.get('/login', login.login);
-    app.post('/login',login.post);
-
-}
